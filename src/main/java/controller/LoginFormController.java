4b608f26efc8d5e12e469da5078bc44229f546c6
Kavindu Dilshan <kavindu4543@gmail.com>
2025-02-08 19:09:43 +0530

    Project initilaization and Login UI

---
+++ src/main/java/controller/LoginFormController.java
@@ -0,0 +1,41 @@
+package controller;
+
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class LoginFormController {
+
+    @FXML
+    private JFXCheckBox chkCheckbox1;
+
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    @FXML
+    private JFXTextField txtUsername;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void hyperRegisterOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void showPswOnMousePressed(MouseEvent event) {
+
+    }
+
+    @FXML
+    void showPswOnMouseReleased(MouseEvent event) {
+
+    }
+
+}
---
+++ src/main/java/main/Starter.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Starter extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/login.fxml"))));
+        stage.setResizable(false);
+        stage.setTitle("System Login");
+        stage.show();
+    }
+}
